ab6b62ba82fdafe094b40f86eca685fb702e05dc
izabellllia <elizavetazhitnik@mail.ru>
2023-11-15 11:37:52 +0700

    fix branch

--- Campus.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS campus CASCADE;
-
-CREATE TABLE campus(domitory_number text);
-
-INSERT INTO campus(domitory_number)
-VALUES('1A'), ('1B'), ('4'), ('5'), ('6'), ('7'), ('8/1'), ('8/2'), ('9'), ('10');
-
-SELECT * FROM campus;
