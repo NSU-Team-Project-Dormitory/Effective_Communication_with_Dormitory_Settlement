7dea0d0573f4d88d8e42c624d413e2c0c5c1b752
izabellllia <elizavetazhitnik@mail.ru>
2023-11-21 21:22:47 +0700

    add to Building5Factory floors(2,3,4,5) and all  rooms for first floor

--- CampusProject/CampusProject/NsuCampusFactory/Building5/Building5Factory.cs
+++ CampusProject/CampusProject/NsuCampusFactory/Building5/Building5Factory.cs
@@ -10,9 +10,17 @@
     {
         var floors = new Dictionary<Guid, Floor>();
         var firstFloor = new Building5FirstFloorFactory(_model).Create();
+        var secondFloor = new Building5SecondFloorFactory(_model).Create();
+        var thirdFloor = new Building5ThirdFloorFactory(_model).Create();
+        var fourthFloor = new Building5FourthFloorFactory(_model).Create();
+        var fifthFloor = new Building5FifthhFloorFactory(_model).Create();
 
         floors.Add(firstFloor.Id, firstFloor);
-        
+        floors.Add(secondFloor.Id, secondFloor);
+        floors.Add(thirdFloor.Id, thirdFloor);
+        floors.Add(fourthFloor.Id, fourthFloor);
+        floors.Add(fifthFloor.Id, fifthFloor);
+
         var building = new Building(_model, new Guid("{C52285D0-6164-410E-83A2-BC4C2001191D}"),
             "Общежитие 5", floors);
 
