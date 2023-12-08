0edb9e240d06f27d69dafc123bb0976a8d742ca4
NotWhaleee <kozorezn2004@gmail.com>
2023-12-08 13:38:10 +0700

    students factory, not working

---
+++ CampusProject/CampusProject/StudentsFactory/StudentsFactory.cs
@@ -0,0 +1,29 @@
+﻿public sealed class StudentsFactory
+{
+    private readonly Students _model;
+
+    public StudentsFactory(Students model)
+    {
+        _model = model;
+    }
+    public Students Create()
+    {
+        var floors = new Dictionary<Guid, Floor>();
+        var firstFloor = new Building5FirstFloorFactory(_model).Create();
+
+        var rooms = new Dictionary<Guid, Room>();
+
+        var room100 = new Room(_model, new Guid("{d904752a-0867-4207-866e-1b54d0db5c44}"),
+            "100");
+       
+        rooms.Add(room100.Id, room100);
+        
+        return new Floor(_model, new Guid("{DD656AAB-EFC8-4E35-947B-89284E781A6B}")
+            , "Этаж 1", rooms);
+
+        var building = new Students(_model, new Guid("{C52285D0-6164-410E-83A2-BC4C2001191D}"),
+            "Общежитие 5", floors);
+
+        return studets;
+    }
+}
