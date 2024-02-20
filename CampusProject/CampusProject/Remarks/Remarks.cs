30e92df2ff5654ec4a23d4fe53c287cb7bb9be5f
Liza Zhitnik <e.zhitnik@g.nsu.ru>
2024-02-20 11:51:46 +0700

    addRemarksToRoom

---
+++ CampusProject/CampusProject/Remarks/Remarks.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CampusProject
+{
+    public enum NoteType
+    {
+        Cleaning,
+        FireSafety,
+        Behavior,
+        Custom
+    }
+
+    public class RoomRemarks
+    {
+        private List<string> remarks;
+
+        public RoomRemarks()
+        {
+            remarks = new List<string>();
+        }
+
+        public void AddRemark(NoteType type, string content, int noteNumber)
+        {
+            remarks.Add($"{type.ToString()}: {noteNumber} - {content}");
+        }
+
+        public void ViewRemarks()
+        {
+            if (remarks.Count == 0)
+            {
+                Console.WriteLine("Замечаний пока нет");
+            }
+            else
+            {
+                Console.WriteLine("Замечания:");
+                foreach (var remark in remarks)
+                {
+                    Console.WriteLine(remark);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            RoomRemarks roomRemarks = new RoomRemarks();
+
+            Console.WriteLine("Выберите тип замечания:");
+            Console.WriteLine("1. Уборка");
+            Console.WriteLine("2. Пожарная безопасность");
+            Console.WriteLine("3. Поведение");
+            Console.WriteLine("4. Написать замечание самостоятельно");
+            int choice = int.Parse(Console.ReadLine());
+
+            NoteType type;
+
+            switch (choice)
+            {
+                case 1:
+                    type = NoteType.Cleaning;
+                    break;
+                case 2:
+                    type = NoteType.FireSafety;
+                    break;
+                case 3:
+                    type = NoteType.Behavior;
+                    break;
+                case 4:
+                    type = NoteType.Custom;
+                    break;
+                default:
+                    throw new ArgumentException("Неверный выбор типа замечания");
+            }
+
+            Console.WriteLine("Введите номер замечания:");
+            int noteNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите содержимое замечания:");
+            string content = Console.ReadLine();
+
+            roomRemarks.AddRemark(type, content, noteNumber);
+            roomRemarks.ViewRemarks();
+        }
+    }
+
+    public class Room
+    {
+        public string Number { get; set; }
+        public List<string> Remarks { get; set; }
+
+        public Room(string number)
+        {
+            Number = number;
+            Remarks = new List<string>();
+        }
+    }
+
+    public class Floor
+    {
+        public List<Room> Rooms { get; set; }
+
+        public Floor()
+        {
+            Rooms = new List<Room>();
+        }
+    }
+}
--- CampusProject/CampusProject/SelectRoom.cs
+++ CampusProject/CampusProject/SelectRoom.cs
@@ -13,12 +13,67 @@ namespace CampusProject
 
             if (RoomExists(building5FirstFloorFactory, roomNumber))
             {
-                Console.WriteLine("СЮДА ВЫВОДИТЬ ЭТИХ ЗАСЕЛЕНЦЕВ");
+                Console.WriteLine($"Выбрана комната {roomNumber}");
+                ShowRemarksMenu(roomNumber); // Показать меню с возможностью добавления и просмотра замечаний
             }
             else
             {
                 Console.WriteLine($"Комната {roomNumber} не существует.");
             }
+        }
+
+        private static void ShowRemarksMenu(string roomNumber)
+        {
+            RoomRemarks remarks = new RoomRemarks();
+            while (true)
+            {
+                Console.WriteLine("1. Добавить замечание");
+                Console.WriteLine("2. Просмотреть замечания");
+                Console.WriteLine("3. Выйти");
+                Console.WriteLine("Выберите действие: ");
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddRemark(remarks);
+                        break;
+                    case "2":
+                        ViewRemarks(remarks);
+                        break;
+                    case "3":
+                        return;
+                    default:
+                        Console.WriteLine("Недопустимый выбор");
+                        break;
+                }
+            }
+        }
+
+        private static void AddRemark(RoomRemarks remarks)
+        {
+            Console.WriteLine("Выберите тип замечания (Cleaning, FireSafety, Behavior, Custom): ");
+            var typeString = Console.ReadLine();
+            NoteType type;
+            if (Enum.TryParse(typeString, out type))
+            {
+                Console.WriteLine("Введите содержание замечания: ");
+                var content = Console.ReadLine();
+                Console.WriteLine("Введите номер замечания: ");
+                var noteNumber = Convert.ToInt32(Console.ReadLine());
+
+                remarks.AddRemark(type, content, noteNumber);
+                Console.WriteLine("Замечание добавлено");
+            }
+            else
+            {
+                Console.WriteLine("Недопустимый тип замечания");
+            }
+        }
+
+        private static void ViewRemarks(RoomRemarks remarks)
+        {
+            remarks.ViewRemarks();
         }
 
         private static bool RoomExists(Building5FirstFloorFactory buildingFactory, string roomNumber)
