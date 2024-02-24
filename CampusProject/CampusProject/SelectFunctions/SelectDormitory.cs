f61d1d3e86ba2b0305c8efa474616a2321cbe286
Liza Zhitnik <e.zhitnik@g.nsu.ru>
2024-02-24 11:39:49 +0700

    add navigation in dormitory selection

--- CampusProject/CampusProject/SelectFunctions/SelectDormitory.cs
+++ CampusProject/CampusProject/SelectFunctions/SelectDormitory.cs
@@ -15,47 +15,51 @@ namespace CampusProject
 
             var nsuBuildingsFactory = new NsuBuildingsFactory(model);
             var buildings = nsuBuildingsFactory.CreateBuildings();
+           
 
             foreach (var building in buildings.Values)
             {
                 Console.WriteLine(building.Name);
             }
 
+            Console.WriteLine("Выйти");
             Console.WriteLine("Введите номер общежития:");
             string inputNumberDormitory = Console.ReadLine();
-
-            switch (inputNumberDormitory)
+            while (true)
             {
-                case "Общежитие 4":
-                case "4":
-                    Console.WriteLine("Выбрано общежитие 4");
-                    break;
-                case "Общежитие 5":
-                case "5":
-                    Console.WriteLine("Выбрано общежитие 5");
-                    Console.WriteLine("Выберите следующее действие: room/student");
-                    string way = Console.ReadLine();
-                    if (way == "room")
-                    {
-                        SelectRoom.Select();
-                    }
-                    else if (way == "student")
-                    {
-                        StudentsInterface.Select();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Неправильный выбор.");
-                    }
-                    break;
+                switch (inputNumberDormitory)
+                {
+                    case "Общежитие 4":
+                    case "4":
+                        Console.WriteLine("Выбрано общежитие 4");
+                        break;
+                    case "Общежитие 5":
+                    case "5":
+                        Console.WriteLine("Выбрано общежитие 5");
+                        Console.WriteLine("Выберите следующее действие: room/student");
+                        string way = Console.ReadLine();
+                        if (way == "room")
+                        {
+                            SelectRoom.Select();
+                        }
+                        else if (way == "student")
+                        {
+                            StudentsInterface.Select();
+                        }
+                        break;
 
-                case "Общежитие 7":
-                case "7":
-                    Console.WriteLine("Выбрано общежитие 7");
-                    break;
-                default:
-                    Console.WriteLine("Неправильный выбор");
-                    break;
+                    case "Общежитие 7":
+                    case "7":
+                        Console.WriteLine("Выбрано общежитие 7");
+                        break;
+                    default:
+                        Console.WriteLine("Неправильный выбор");
+                        Message();
+                        break;
+                    case "Выйти":
+                        Console.WriteLine("Вы вышли из системы");
+                        return;
+                }
             }
         }
     }
