967964fd645d3595d8cbbbbc76e9d395e3148ec1
NotWhaleee <kozorezn2004@gmail.com>
2024-05-26 21:47:47 +0700

    prettier windows

--- DormitoryCommunicationApp/Presentation/View/AddNewStudentWindow.xaml.cs
+++ DormitoryCommunicationApp/Presentation/View/AddNewStudentWindow.xaml.cs
@@ -23,5 +23,6 @@ namespace Presentation.View
         {
             InitializeComponent();
         }
+
     }
 }
--- DormitoryCommunicationApp/Presentation/View/EditStudentWindow.xaml.cs
+++ DormitoryCommunicationApp/Presentation/View/EditStudentWindow.xaml.cs
@@ -23,5 +23,7 @@ namespace Presentation.View
         {
             InitializeComponent();
         }
+
     }
+
 }
